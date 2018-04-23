eacfc7526f67043ebb53b2cda34fee352f17f534
Ethan Lin <ethan.lin.05@gmail.com>
2018-04-23 09:43:27 -0500

    fixed footer vertical alignment on Safari and Chrome

--- footer.php
+++ footer.php
@@ -9,17 +9,17 @@
 			<?php } ?>
 			
 			<!-- footer -->
-			<footer class="footer d-flex align-content-center flex-wrap" role="contentinfo">
+			<footer class="footer d-flex flex-wrap" role="contentinfo">
                 
-                <div class="container">
+                <div class="container d-flex align-items-center flex-wrap">
                     
                     <div class="row">
                         
-                        <div class="col-1 d-flex align-content-center flex-wrap">
+                        <div class="col-1 d-flex align-items-center flex-wrap">
                             <img class="footer-logo" src="<?php echo get_option('footer_logo_option'); ?>" alt="UWEX Media Services" />
                         </div>
                         
-                        <div class="col-9 d-flex align-content-center flex-wrap">
+                        <div class="col-9 d-flex align-items-center flex-wrap">
                             
                             <!-- copyright -->
             				<p class="copyright">&copy; <?php echo date('Y'); ?>.  <?php echo get_option('copyright_option'); ?></p>
@@ -27,7 +27,7 @@
                             
                         </div>
                         
-                        <div class="col-2 d-flex align-content-center flex-wrap">
+                        <div class="col-2 d-flex align-items-center flex-wrap">
                             
                             <?php if ( is_user_logged_in() ) {
                                 
