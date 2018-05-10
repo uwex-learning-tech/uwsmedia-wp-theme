ab57ce2fd0ee0a0761aa47dbf4eeacb3bacabb6a
Ethan Lin <ethan.lin.05@gmail.com>
2018-05-10 14:11:09 -0500

    Single project page code clean up

--- single-uws-projects.php
+++ single-uws-projects.php
@@ -1,88 +1,113 @@
-<?php get_header(); ?>
-    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php 
+    
+    get_header();
+    
+    if ( have_posts() ):
+        
+        while ( have_posts() ) :
             
-           <?php $postID = $post->ID; ?>
+            the_post();
             
-            <!-- post thumbnail -->
-            <?php if ( get_post_meta( $post->ID, 'hide_thumbnail' )[0] != '1' ) : // Check if Thumbnail exists ?>
-    		<?php if ( has_post_thumbnail() ) : // Check if Thumbnail exists ?>
-    		<div class="post-featured-image">
+            $postID = $post->ID;
+            
+            // post thumbnail
+            if ( get_post_meta( $post->ID, 'hide_thumbnail' )[0] != '1' ) :
+            
+                if ( has_post_thumbnail() ) :
+    
+?>
+            
+            <div class="post-featured-image">
     			<?php the_post_thumbnail(); ?>
             </div>
-    		<?php endif; ?>
-    		<?php endif; ?>
-    		<!-- /post thumbnail -->
             
+<?php
+                endif;
             
-            <?php 
-                            
-                // get embedded element if any
-                $embedMedia = get_post_meta( $post->ID, 'media_embed_code', true );
-                
-                if ( !empty( trim( $embedMedia ) ) ) : ?>
-                
-                <div class="top-embed">
-                    <div class="container">
-                         <?php echo $embedMedia; ?>
-                    </div>
+            endif; // end post thumbnail
+            
+            $embedMedia = get_post_meta( $post->ID, 'media_embed_code', true );
+            
+            if ( !empty( trim( $embedMedia ) ) ) :
+            
+?>
+    		
+    		<div class="top-embed">
+        		
+                <div class="container">
+                     <?php echo $embedMedia; ?>
                 </div>
-                    
-                <?php endif; ?>
+                
+            </div>
+            
+            <?php endif; ?>
+
 
             
     <div class="container">
-        <div class="row">
-            <main class="col-12 col-sm-12 col-md-10 offset-md-1" role="main">
-                
-                <!-- section -->
-                <section>
-<!--                     https://pippinsplugins.com/create-live-search-wordpress-jquery-ajax/ -->
-                    <!-- article -->
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        
-                        <!-- post title -->
-                        <h1><?php the_title(); ?></h1>
-                        <!-- /post title -->
-                        
-                        <p class="classification"><strong><?php 
-
-                                $classification_terms = get_the_terms( $post->ID, 'classifications' );
             
-                                if ( !is_array( $classification_terms ) || count( $classification_terms ) <= 0 ) {
-                                    echo '<span aria-hidden="true">&nbsp;</span>';
-                                } else {
-                                    echo $classification_terms[0]->name;
-                                }
-                                
-                            ?></strong></p>
+        <main class="row" role="main">
+                
+            <!-- section -->
+            <section class="col-12 col-sm-12 col-md-10 offset-md-1">
+                
+                <!-- article -->
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    
+                    <!-- share -->
+                    <div class="sharings" role="group" aria-label="Share">
                         
+                        <a id="copy-share-link" class="btn btn-link" href="javascript:void(0);" title="Copy Link"><span class="fa fa-link" aria-hidden="true" aria-hidden="true"></span><input class="hiddenShareLink" type="text" value="<?php the_permalink(); ?>" /></a>
+                        
+                        <a id="shareOnLinkedIn" class="btn btn-link" href="javascript:void(0);" data-ref="http://www.linkedin.com/shareArticle?mini=true&amp;title=<?php echo urlencode(get_the_title()); ?>&amp;summary=<?php echo urlencode(get_the_excerpt()); ?>&amp;url=<?php the_permalink(); ?>&amp;source=<?php echo urlencode( get_blogInfo( 'name' ) ); ?>" title="Share on LinkedIn"><span class="fa fa-linkedin-square" aria-hidden="true"></span></a>
+                        
+                        <div class="share msg"></div>
+                    
+                    </div>
+                    <!-- /share -->
+                    
+                    <!-- post title -->
+                    <h1><?php the_title(); ?></h1>
+                    <!-- /post title -->
+                        
+                    <p class="classification"><strong><?php 
+
+                        $class_terms = get_the_terms( $post->ID, 'classifications' );
+            
+                        if ( is_array( $class_terms )
+                        && count( $class_terms ) >= 1 ) {
+                            
+                            echo $class_terms[0]->name;
+                            
+                        }
+
+                        ?></strong></p>
+                        
+                        <!-- post content -->
                         <?php the_content(); // Dynamic Content ?>
+                        <!-- /post content -->
                         
                         <!-- instructor -->
-        
                         <?php
                             
-                            $otherAuthors = trim( get_post_meta( $post->ID, 'other_authors', true ) );
+                            $instructorStr = trim( get_post_meta( $post->ID, 'other_authors', true ) );
                             
-                            if ( !empty( $otherAuthors ) ) {
+                            if ( !empty( $instructorStr ) ) {
                                 
                                 echo '<p class="instructor"><strong>Instructor:</strong> ';
                                 
-                                if ( !empty( $otherAuthors ) ) {
+                                $instructors = explode( ',', $instructorStr );
+                                $count = 0;
+                                
+                                foreach( $instructors as $instructor ) {
+                                
+                                    echo ucwords( trim( $instructor ) );
                                     
-                                    $otherArry = explode( ',', $otherAuthors );
+                                    $count++;
                                     
-                                    $count = 0;
-                                    
-                                    foreach( $otherArry as $other ) {
-                                    
-                                        echo ucwords( trim( $other ) );
+                                    if ( $count < count( $instructors ) ) {
                                         
-                                        $count++;
-                                        
-                                        if ( $count < count( $otherArry ) ) {
-                                            echo ', ';
-                                        }
+                                        echo ', ';
                                         
                                     }
                                     
@@ -93,28 +118,30 @@
                             }
                             
                         ?>
+                        <!-- /instructor -->
                         
                         <!-- members -->
-                        
                         <?php
                             
-                            $authors = get_post_meta( $post->ID, 'project_authors', true );
+                            $memberStr = get_post_meta( $post->ID, 'project_authors', true );
                             
-                            if ( !empty( $authors ) ) {
+                            if ( !empty( $memberStr ) ) {
                                 
                                 echo '<p class="members"><strong>Team Member(s):</strong> ';
                             
+                                $memberIds = explode( ',', $memberStr );
                                 $count = 0;
-                                $authorIds = explode( ',', get_post_meta( $post->ID, 'project_authors', true ) );
                                 
-                                foreach( $authorIds as $id ) {
+                                foreach( $memberIds as $id ) {
                                 
                                     echo '<a href="' . get_the_permalink( $id ) .'">' . get_the_title( $id ) . '</a>';
                                     
                                     $count++;
                                     
-                                    if ( $count < count( $authorIds ) ) {
+                                    if ( $count < count( $memberIds ) ) {
+                                        
                                         echo ', ';
+                                        
                                     }
                                     
                                 }
@@ -124,9 +151,9 @@
                             }
                             
                         ?>
+                        <!-- /members -->
                         
-                        
-                        
+                        <!-- tags -->
                         <ul class="tag-pills">
                         <?php 
 
@@ -163,27 +190,16 @@
 
                         ?>
                         </ul>
-                        
-                        <!-- share -->
-                            
-                            <p class="shareHeading">Share:</p>
-                            <div class="sharings btn-group" role="group" aria-label="Share">
-                                
-                                <a id="copy-share-link" class="btn btn-outline-secondary" href="javascript:void(0);" title="Copy Link"><span class="fa fa-link" aria-hidden="true" aria-hidden="true"></span><input class="hiddenShareLink" type="text" value="<?php the_permalink(); ?>" /></a>
-                                
-                                <a id="shareOnLinkedIn" class="btn btn-outline-secondary" href="javascript:void(0);" data-ref="http://www.linkedin.com/shareArticle?mini=true&amp;title=<?php echo urlencode(get_the_title()); ?>&amp;summary=<?php echo urlencode(get_the_excerpt()); ?>&amp;url=<?php the_permalink(); ?>&amp;source=<?php echo urlencode( get_blogInfo( 'name' ) ); ?>" title="Share on LinkedIn"><span class="fa fa-linkedin-square" aria-hidden="true"></span></a>
-                            
-                            </div>
-                            <div class="share msg"></div>
+                        <!-- /tags -->
                         
                         <?php edit_post_link( __( 'Edit Project', 'uwsmedia' ), '<p>', '</p>', $postID ); ?>
                     
                     </article>
                     <!-- /article -->
                     
-                    <?php endwhile; ?>
+        <?php endwhile; ?>
                     
-                    <?php else: ?>
+    <?php else: ?>
                     
                     <!-- article -->
                     <article>
@@ -193,15 +209,14 @@
                     </article>
                     <!-- /article -->
                     
-                    <?php endif; ?>
+    <?php endif; ?>
                 
                 </section>
                 <!-- /section -->
+                
             </main>
-			    
-        </div>
+
 	</div>
 	<!-- /body content container -->
-
 
 <?php get_footer(); ?>
