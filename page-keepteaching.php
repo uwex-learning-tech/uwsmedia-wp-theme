e5f39767aeeb62794560c6ce2f9b56936b8f08d1
Ethan Lin <ethan.lin@uwex.edu>
2020-07-24 12:33:15 -0500

    Update page title for keep teaching page

--- page-keepteaching.php
+++ page-keepteaching.php
@@ -13,7 +13,7 @@
 <html <?php language_attributes(); ?> class="no-js<?php echo is_404() ? ' error404' : '';?>" style="margin-top: 0 !important;">
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ''; } ?></title>
+	<title>UWEX - <?php wp_title(''); ?><?php if(wp_title('', false)) { echo ''; } ?></title>
 
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
     <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
