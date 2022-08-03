94c380d07790aeae9d4162eb281996993b31aa79
Ethan Lin <ethan.lin@uwex.wisconsin.edu>
2022-08-03 10:44:58 -0500

    updated 404 page message

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@
                     <h1 class="text-center">Page Not Found</h1>
                     <p class="text-center">The requested page is not found or not available at the moment.</p>
                     <div class="alert alert-light" role="alert">
-                        <p>For assistance related to course materials or contents, please submit the form on the Technical Support website at <a href="https://ce.uwex.edu/technical-support" target="_blank">ce.uwex.edu/technical-support</a>, or call <a href="tel:1-877-724-7883">1-877-724-7883</a>.</p>
+                        <p>For assistance related to course materials, please submit a ticket through the <a href="https://ce.uwex.edu/technical-support" target="_blank">Technical Support website</a> or call <a href="tel:1-877-724-7883">1-877-724-7883</a>.</p>
                         <hr>
                         <p class="mb-0 text-center"><?php
                     if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
